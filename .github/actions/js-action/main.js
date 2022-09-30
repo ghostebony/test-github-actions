25d9d41da7befbfa6096145909fa229fa848e0c8
ghostebony <pedroamericolouzada@uol.com.br>
2022-09-30 12:38:44 -0300

    github action test 13

--- .github/actions/js-action/main.js
+++ .github/actions/js-action/main.js
@@ -4,11 +4,15 @@ import { request } from 'undici';
 
 const pkg = JSON.parse(readFileSync('./package.json').toString());
 
-const currentVersion = pkg.devDependencies.svelte;
+const currentVersion = pkg.devDependencies.svelte.replace(/\W+/g, '');
 
 const { body } = await request('https://registry.npmjs.org/svelte');
 const data = await body.json();
 
-const latestVersion = data['dist-tags'].latest;
+const latestVersion = data['dist-tags'].latest.replace(/\W+/g, '');
 
-console.log({ currentVersion, latestVersion });
+console.log(
+	{ currentVersion, latestVersion },
+	'publish new version?',
+	currentVersion !== latestVersion
+);
